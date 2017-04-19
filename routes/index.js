2dee155c2b629d6baf78d37d459891b862796760
huangweiqiang <mrhwq@foxmail.com>
2017-04-19 09:13:32 +0800

    add the install and run cmd, remove the mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-var mongoose = require('mongoose');
-var db;
+// var mongoose = require('mongoose');
+// var db;
 // if (process.env.VCAP_SERVICES) {
 //     var env = JSON.parse(process.env.VCAP_SERVICES);
 //     db = mongoose.createConnection(env['mongodb-2.2'][0].credentials.url);
 // } else {
-db = mongoose.createConnection('localhost', 'pollsapp');
+// db = mongoose.createConnection('localhost', 'pollsapp');
 // }
-db.on('error', console.error.bind(console, '连接错误:'));
-db.once('open', function () {
-    //一次打开记录
-    console.log("the mongoose is open");
-});
+// db.on('error', console.error.bind(console, '连接错误:'));
+// db.once('open', function () {
+//     //一次打开记录
+//     console.log("the mongoose is open");
+// });
 
 // Get Poll schema and model
-var PollSchema = require('../models/pollmodel.js').PollSchema;
-var PollEntity = db.model('polls', PollSchema);
+// var PollSchema = require('../models/pollmodel.js').PollSchema;
+// var PollEntity = db.model('polls', PollSchema);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -32,9 +32,9 @@ router.get('/', function (req, res, next) {
 // JSON API for list of polls
 router.get('/polls/polls', function (req, res, next) {
     // Query Mongo for polls, just get back the question text
-    PollEntity.find({}, 'question', function (error, polls) {
-        res.json(polls);
-    });
+    // PollEntity.find({}, 'question', function (error, polls) {
+    //     res.json(polls);
+    // });
 });
 
 router.get('/polls/:id', function (req, res, next) {
@@ -42,100 +42,100 @@ router.get('/polls/:id', function (req, res, next) {
     var pollId = req.params.id;
 
     // Find the poll by its ID, use lean as we won't be changing it
-    PollEntity.findById(pollId, '', {lean: true}, function (err, poll) {
-        if (poll) {
-            var userVoted = false,
-                userChoice,
-                totalVotes = 0;
-
-            // Loop through poll choices to determine if user has voted
-            // on this poll, and if so, what they selected
-            for (c in poll.choices) {
-                var choice = poll.choices[c];
-
-                for (v in choice.votes) {
-                    var vote = choice.votes[v];
-                    totalVotes++;
-
-                    if (vote.ip === (req.header('x-forwarded-for') || req.ip)) {
-                        userVoted = true;
-                        userChoice = {_id: choice._id, text: choice.text};
-                    }
-                }
-            }
-
-            // Attach info about user's past voting on this poll
-            poll.userVoted = userVoted;
-            poll.userChoice = userChoice;
-            poll.totalVotes = totalVotes;
-
-            res.json(poll);
-        } else {
-            res.json({error: true});
-        }
-    });
+    // PollEntity.findById(pollId, '', {lean: true}, function (err, poll) {
+    //     if (poll) {
+    //         var userVoted = false,
+    //             userChoice,
+    //             totalVotes = 0;
+    //
+    //         // Loop through poll choices to determine if user has voted
+    //         // on this poll, and if so, what they selected
+    //         for (c in poll.choices) {
+    //             var choice = poll.choices[c];
+    //
+    //             for (v in choice.votes) {
+    //                 var vote = choice.votes[v];
+    //                 totalVotes++;
+    //
+    //                 if (vote.ip === (req.header('x-forwarded-for') || req.ip)) {
+    //                     userVoted = true;
+    //                     userChoice = {_id: choice._id, text: choice.text};
+    //                 }
+    //             }
+    //         }
+    //
+    //         // Attach info about user's past voting on this poll
+    //         poll.userVoted = userVoted;
+    //         poll.userChoice = userChoice;
+    //         poll.totalVotes = totalVotes;
+    //
+    //         res.json(poll);
+    //     } else {
+    //         res.json({error: true});
+    //     }
+    // });
 });
 
 // JSON API for creating a new poll
 router.post('/polls', function (req, res, next) {
-    console.log("newpolls");
-    var reqBody = req.body;
-    // Filter out choices with empty text
-    var choices = reqBody.choices.filter(function (v) {
-        return v.text != '';
-    });
-    // Build up poll object to save
-    var pollObj = {question: reqBody.question, choices: choices};
-
-    // Create poll model from built up poll object
-    var poll = new PollEntity(pollObj);
-
-    // Save poll to DB
-    poll.save(function (err, doc) {
-        if (err || !doc) {
-            throw 'Error';
-        } else {
-            res.json(doc);
-        }
-    });
+    // console.log("newpolls");
+    // var reqBody = req.body;
+    // // Filter out choices with empty text
+    // var choices = reqBody.choices.filter(function (v) {
+    //     return v.text != '';
+    // });
+    // // Build up poll object to save
+    // var pollObj = {question: reqBody.question, choices: choices};
+    //
+    // // Create poll model from built up poll object
+    // var poll = new PollEntity(pollObj);
+    //
+    // // Save poll to DB
+    // poll.save(function (err, doc) {
+    //     if (err || !doc) {
+    //         throw 'Error';
+    //     } else {
+    //         res.json(doc);
+    //     }
+    // });
 });
 
 var vote = function (socket) {
-    socket.on('send:vote', function (data) {
-        var ip = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address.address;
-
-        PollEntity.findById(data.poll_id, function (err, poll) {
-            var choice = poll.choices.id(data.choice);
-            choice.votes.push({ip: ip});
-
-            poll.save(function (err, doc) {
-                var theDoc = {
-                    question: doc.question, _id: doc._id, choices: doc.choices,
-                    userVoted: false, totalVotes: 0
-                };
-
-                // Loop through poll choices to determine if user has voted
-                // on this poll, and if so, what they selected
-                for (var i = 0, ln = doc.choices.length; i < ln; i++) {
-                    var choice = doc.choices[i];
-
-                    for (var j = 0, jLn = choice.votes.length; j < jLn; j++) {
-                        var vote = choice.votes[j];
-                        theDoc.totalVotes++;
-                        theDoc.ip = ip;
-
-                        if (vote.ip === ip) {
-                            theDoc.userVoted = true;
-                            theDoc.userChoice = {_id: choice._id, text: choice.text};
-                        }
-                    }
-                }
-
-                socket.emit('myvote', theDoc);
-                socket.broadcast.emit('vote', theDoc);
-            });
-        });
-    });
+    // socket.on('send:vote', function (data) {
+    //     var ip = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address.address;
+    //
+    //     PollEntity.findById(data.poll_id, function (err, poll) {
+    //         var choice = poll.choices.id(data.choice);
+    //         choice.votes.push({ip: ip});
+    //
+    //         poll.save(function (err, doc) {
+    //             var theDoc = {
+    //                 question: doc.question, _id: doc._id, choices: doc.choices,
+    //                 userVoted: false, totalVotes: 0
+    //             };
+    //
+    //             // Loop through poll choices to determine if user has voted
+    //             // on this poll, and if so, what they selected
+    //             for (var i = 0, ln = doc.choices.length; i < ln; i++) {
+    //                 var choice = doc.choices[i];
+    //
+    //                 for (var j = 0, jLn = choice.votes.length; j < jLn; j++) {
+    //                     var vote = choice.votes[j];
+    //                     theDoc.totalVotes++;
+    //                     theDoc.ip = ip;
+    //
+    //                     if (vote.ip === ip) {
+    //                         theDoc.userVoted = true;
+    //                         theDoc.userChoice = {_id: choice._id, text: choice.text};
+    //                     }
+    //                 }
+    //             }
+    //
+    //             socket.emit('myvote', theDoc);
+    //             socket.broadcast.emit('vote', theDoc);
+    //         });
+    //     });
+    // });
 };
 
 router.post('/vote', vote);
